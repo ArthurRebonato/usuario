3c24f9053dcc7ed9313b11fd1651d2245e04eee4
ArthurRebonato <arthur.rebonato@hotmail.com>
2026-05-06 16:30:18 -0300

    Adicionados métodos get, delete e login

---
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.javanauta.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable cause){
+        super(mensagem,cause);
+    }
+}
